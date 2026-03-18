d542a6694a4178ede2bfe0a464e2931977228b22
Sravani Reddy <kistapatisravanireddy@gmail.com>
2026-03-18 23:18:55 +0530

    Updated

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -1,7 +1,5 @@
 from google import genai
-import os
-
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+api_key="AIzaSyCuBsAd3hysDpR3P3FJEcckKCYENCERMVw"
 
 def analyze_data(data: str, sector: str):
     try:
