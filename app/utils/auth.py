dd35b45e7aec201953a6f4125350a6b06382316c
Sravani Reddy Kistapati <142100126+Sravani-1309@users.noreply.github.com>
2026-03-19 00:46:20 +0530

    Update auth.py

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,9 +1,12 @@
 from fastapi import Header, HTTPException
 
-def verify_user(x_api_key: str = Header(None, alias="x-api-key"),
-               api_key: str=None):
-    print("Received API KEY:", api_key)
-    key = x_api_key or api_key               
+def verify_user(
+    x_api_key: str = Header(None, alias="x-api-key"),
+    api_key: str = None
+):
+    key = x_api_key or api_key
+
     if key != "mysecretkey":
         raise HTTPException(status_code=401, detail="Unauthorized")
+
     return key
